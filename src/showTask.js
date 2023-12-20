5a516fd82992e1ad1af0d8d0bfce0379aee87f08
Cross <118707038+llCross@users.noreply.github.com>
2023-12-21 01:31:07 +0800

    Add showTask module for rendering tasks

---
+++ src/showTask.js
@@ -0,0 +1,38 @@
+import { myTask } from "./taskClass";
+
+export default function showTask() {
+
+    let taskContainer = document.querySelector('.task-container');
+
+    taskContainer.innerText = '';
+
+    for (let i = 0; i < myTask.length; i++) {
+        let task = myTask[i];
+
+        let taskContent = document.createElement('div');
+        taskContent.classList.add('task');
+        
+        let titleContainer = document.createElement('div');
+        titleContainer.classList.add('title-container');
+
+        let taskCheckbox = document.createElement("input");
+        taskCheckbox.setAttribute("type", "checkbox");
+        taskCheckbox.addEventListener('click', () => {
+            myTask[i].completed();
+        });
+
+        let taskTitle = document.createElement('p');
+        taskTitle.innerText = `${task.title}`;
+        taskTitle.classList.add('title');
+
+        let taskDate = document.createElement('p');
+        taskDate.innerText = `${task.dueDate}`;
+        taskDate.classList.add('due-date');
+
+        taskContainer.appendChild(taskContent);
+        taskContent.appendChild(titleContainer);
+        titleContainer.appendChild(taskCheckbox);
+        titleContainer.appendChild(taskTitle);
+        taskContent.appendChild(taskDate);
+    }
+}
